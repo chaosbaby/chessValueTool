d02b8e4ecc91f3d9ea1271c38e807ee73823f77a
chaosbaby <50092526@qq.com>
2019-12-04 11:24:17 +0800

    add base setup for build

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='chessValueTool',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    entry_points="""
+    [console_scripts]
+    """,
+)
